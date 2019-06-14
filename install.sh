0c5bd54e5f5cb35a2bf333906ab7de4925ed47ec
0delta <0deltast@gmail.com>
2019-06-14 23:52:43 +0900

    fix can't install with script

--- install.sh
+++ install.sh
@@ -1,18 +1,33 @@
-cd vim/src
-./configure \
-    --enable-fail-if-missing \
-    --disable-gui \
-    --enable-pythoninterp=dynamic \
-    --enable-rubyinterp=dynamic \
-    --enable-cscope \
-    --disable-netbeans \
-    --enable-multibyte \
-    --disable-xim \
-    --disable-sysmouse \
-    --with-features=huge \
-    --without-x \
-    --disable-xsmp \
-    --with-compiledby=0Delta \
-    --prefix=~/bin/vim
+#!/bin/bash
+CONFIG=()
+CONFIG+=("enable-fail-if-missing")
+CONFIG+=("disable-gui")
+# CONFIG+=("enable-pythoninterp=dynamic")
+# CONFIG+=("with-python-command=/usr/local/bin/python2")
+# CONFIG+=("enable-python3interp=dynamic")
+# CONFIG+=("with-python3-command=/usr/local/bin/python3")
+# CONFIG+=("enable-rubyinterp")
+CONFIG+=("enable-luainterp")
+CONFIG+=("with-lua-prefix=/usr/local")
+CONFIG+=("enable-cscope")
+CONFIG+=("disable-netbeans")
+CONFIG+=("enable-multibyte")
+CONFIG+=("disable-xim")
+CONFIG+=("disable-sysmouse")
+CONFIG+=("with-features=huge")
+CONFIG+=("without-x")
+CONFIG+=("disable-xsmp")
+CONFIG+=("with-compiledby=0Delta")
+CONFIG+=("prefix=$HOME/bin/vim")
 
-make build && make install
+CONFIG_CMD="./configure"
+for v in "${CONFIG[@]}"
+do
+  CONFIG_CMD+=" --${v}"
+done
+
+
+cd vim/src
+make distclean
+eval ${CONFIG_CMD}
+make -j $(sysctl -n hw.ncpu) && make install
